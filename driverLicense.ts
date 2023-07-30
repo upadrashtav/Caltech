6d88f24a9f067368535128715fdb200a0d3f668b
upadrashtav <vupadrashta@yahoo.com>
2023-07-30 10:00:13 -0500

    initial commit

---
+++ driverLicense.ts
@@ -0,0 +1,15 @@
+let dl = {
+    "issuedState":"Pennsylvania",
+    "driversLicenseNumber":  "99 999 999"  ,
+    "lastName":"sample",
+    "firstName":"Sample First Name",
+    "address":{
+        "line1":"123 Main Street",
+        "line2":"Apt 123",
+        "city":"WhoKnowns",
+        "state":"PA",
+        "zipcode":"17101-0000"
+    },
+    "organdonor":true,
+    "deps":"00"
+};
